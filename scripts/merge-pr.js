191003cff514afd9af275a339a4b9b4ea6e7b5fd
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-10-26 11:00:21 +0300

    Fix script

--- scripts/merge-pr.js
+++ scripts/merge-pr.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 const pipeline = require('../dist/pr-pipeline');
+const dbpath = path.join(__dirname, '../db');
 const jsonPath = path.join(__dirname, '../pr.json');
 
 if (fs.existsSync(jsonPath)) {
-  pipeline.run(jsonPath, { dbpath: path.join(__dirname, '../db') });
+  pipeline.run({ dbpath, jsonPath });
 } else {
   console.log('Nothing to process');
 }
