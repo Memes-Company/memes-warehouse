031e947dcea9d9ed3c2c71fe3fe77495e768cf31
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-10-29 10:39:10 +0300

    Seed from migrations

--- src/database/migrations/1572303950237-create-memes-table.ts
+++ src/database/migrations/1572303950237-create-memes-table.ts
@@ -1,14 +1,23 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { Meme } from '../entities/meme.entity';
 
+// tslint:disable-next-line: class-name
 export class createMemesTable1572303950237 implements MigrationInterface {
-    name = 'createMemesTable1572303950237'
+  name = 'createMemesTable1572303950237';
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "memes" ("id" varchar PRIMARY KEY NOT NULL, "title" varchar(128) NOT NULL, "description" text NOT NULL)`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "memes" ("id" varchar PRIMARY KEY NOT NULL, "title" varchar(128) NOT NULL, "description" text NOT NULL)`,
+      undefined,
+    );
+    const memes = queryRunner.connection.getRepository(Meme);
+    memes.save({
+      title: 'Seeded title',
+      description: 'Seeded description',
+    });
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`DROP TABLE "memes"`, undefined);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP TABLE "memes"`, undefined);
+  }
 }
