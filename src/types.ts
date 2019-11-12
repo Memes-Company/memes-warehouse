9b31fb777c6b2cf4077777aa6ea6b5a11c5c30fd
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-11-13 01:53:05 +0300

    Add DataSet type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,12 @@
 export type Locale = 'en' | 'ru';
 
+export interface DataSet {
+  PullRequests: { [key: string]: LocaleAwarePullRequest };
+  Memes: WithLocale<Meme[]>;
+  Tags: WithLocale<Tag[]>;
+}
+export type WithLocale<T> = { [key in Locale]: T };
+
 export type LocaleAwarePullRequest = {
   [key in Locale]?: PullRequest;
 } & {
