7bf51a73fe3036f5c18321aaf49578ab54807716
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-11-16 15:05:29 +0300

    Fix

--- src/blocks/remove-pullrequests.ts
+++ src/blocks/remove-pullrequests.ts
@@ -1,8 +1,9 @@
+import path from 'path';
 import rimraf from 'rimraf';
 
 import { LocaleAwarePullRequest, PipelineConfig } from '../types';
 
 export async function removePullrequests(pullRequest: LocaleAwarePullRequest, config: PipelineConfig) {
-  rimraf.sync(config.pullrequestsDir);
+  rimraf.sync(path.join(config.dbpath, 'pull-requests'));
   return pullRequest;
 }
