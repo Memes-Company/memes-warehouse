9c7ac68511dff5d27b51b6cbf6b75285bacf3f2d
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-11-16 15:04:51 +0300

    Remove useless path

--- scripts/merge-pr.js
+++ scripts/merge-pr.js
@@ -2,11 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { PullRequestsPipeline } = require('../dist/pr-pipeline');
 const dbpath = path.join(__dirname, '../db');
-const pullrequestsDir = path.join(__dirname, '../db/pull-requests');
 
-if (fs.existsSync(pullrequestsDir)) {
-  let pipeline = new PullRequestsPipeline({ dbpath, pullrequestsDir });
-  pipeline.run();
-} else {
-  console.log('Nothing to process');
-}
+let pipeline = new PullRequestsPipeline({ dbpath });
+pipeline.run();
