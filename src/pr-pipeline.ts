7ec6c163694218358fcd359946a5e4864900f983
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-12-09 08:24:03 +0300

    Try pify

--- src/pr-pipeline.ts
+++ src/pr-pipeline.ts
@@ -10,6 +10,7 @@ import {
 import { SaveDatabase } from './blocks/save-database';
 import { gitAdd, gitCheckout, gitCommit } from './functions';
 import { PipelineBlock, PipelineConfig } from './types';
+import { resolve } from 'dns';
 
 export class PullRequestsPipeline {
   private blocks: Array<PipelineBlock>;
@@ -23,21 +24,31 @@ export class PullRequestsPipeline {
 
   async run(): Promise<void> {
     let database = await new LoadDataBase(this.config).process(null); // not so beauty as I expected 🤔
-    for await (const id of Object.keys(database.pullRequests)) {
-      const pipeline = database.pullRequests[id];
-      for await (const block of this.blocks) {
-        try {
-          console.log(`Apply ${block.name}...`);
-          database = await block.process(database, pipeline);
-        } catch (error) {
-          console.error(`Pipeline block '${block.name}' failed with exception:`);
-          console.error(error);
-          console.error('when processing pull-request:');
-          console.error(JSON.stringify(pipeline));
-          process.exit(1);
+    await new Promise(async (resolve) => {
+      for (const id of Object.keys(database.pullRequests)) {
+        const pullRequest = database.pullRequests[id];
+        await new Promise(async (resolve) => {
+          for (const block of this.blocks) {
+            try {
+              console.log(`Apply ${block.name}...`);
+              database = await block.process(database, pullRequest);
+              if (this.blocks.slice(-1)[0].name === block.name) {
+                resolve();
+              }
+            } catch (error) {
+              console.error(`Pipeline block '${block.name}' failed with exception:`);
+              console.error(error);
+              console.error('when processing pull-request:');
+              console.error(JSON.stringify(pullRequest));
+              process.exit(1);
+            }
+          }
+        });
+        if (Object.keys(database.pullRequests).slice(-1)[0] === id) {
+          resolve();
         }
       }
-    }
+    });
 
     if (process.env.TRAVIS_BRANCH) {
       await gitCheckout(process.env.TRAVIS_BRANCH);
