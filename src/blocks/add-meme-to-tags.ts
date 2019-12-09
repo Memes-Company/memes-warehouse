928e977d7c8f3b1e912c11e94ec44662bedcbf29
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-12-09 08:55:34 +0300

    WIP

--- src/blocks/add-meme-to-tags.ts
+++ src/blocks/add-meme-to-tags.ts
@@ -4,6 +4,7 @@ export class AddMemeToTags extends PipelineBlock {
   public name: string = AddMemeToTags.name;
   async process(database: DataBase, currentPR: LocaleAwarePullRequest): Promise<DataBase> {
     currentPR.locales.map((locale) => {
+      currentPR[locale].meme.tags = currentPR[locale].meme.tags || []; // null propagation will work?
       currentPR[locale].meme.tags.forEach((tagId) => {
         const tag = database.tags[locale as Locales][tagId];
         tag.memes.push(currentPR[locale].meme.id);
